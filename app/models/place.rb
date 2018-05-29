1ee6fe2d9340e535a29db6799c275bef6af46211
ruby7sapphire <wewillmeet@icloud.com>
2018-05-29 01:25:33 -0700

    Lesson 29.2: Add lat/long fields and geocoding to Places

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,13 @@
 class Place < ApplicationRecord
   belongs_to :user
+
+  def full_address
+    [self.name, self.address].join(' ')
+	end
+
+  geocoded_by :full_address
+  after_validation :geocode
+
   validates :name, :address, :description, presence: true
   validates :name, length: { minimum: 4 }
 end
