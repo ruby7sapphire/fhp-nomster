bee637414b6a85df561b1e3c4e698de68cf96dfb
ruby7sapphire <wewillmeet@icloud.com>
2018-05-31 13:55:34 -0700

    Lesson 31: Add Comments model

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,12 +1,13 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
 
   geocoded_by :full_address
   after_validation :geocode
 
   validates :name, :address, :description, presence: true
   validates :name, length: { minimum: 4 }
-  
+
   def full_address
     [name, address].join(' ')
   end
