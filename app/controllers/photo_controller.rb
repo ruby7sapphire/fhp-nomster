047439ac05f40f9f05bac49f0db3a0937e16522a
ruby7sapphire <wewillmeet@icloud.com>
2018-06-06 23:20:52 -0700

    Lesson 39.2: Install carrierwave gem; add picture field to Photos

--- app/controllers/photo_controller.rb
+++ app/controllers/photo_controller.rb
@@ -10,6 +10,6 @@ class PhotoController < ApplicationController
   private
 
   def photo_params
-    params.require(:photo).permit(:caption)
+    params.require(:photo).permit(:caption, :picture)
   end
 end
