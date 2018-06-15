bd0c2a3a8bdded589c8c39f4056acceab6e270cc
ruby7sapphire <wewillmeet@icloud.com>
2018-06-14 17:55:51 -0700

    Lesson 44: Add SendGrid to production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,12 +55,25 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "nomster_#{Rails.env}"
+
+  # Mail
+
   config.action_mailer.default_url_options = { host: 'https://nomster-ruby7sapphire.herokuapp.com' }
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    enable_starttls_auto: true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
